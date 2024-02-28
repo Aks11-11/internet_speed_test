b712c57f17280bc6608f18770c850ea04332e74a
Aks11-11 <aksshatvashisht@gmail.com>
2024-02-28 17:56:17 +0530

    1st

--- internet 2.0.py
+++ internet 2.0.py
@@ -1,3 +1,43 @@
+from tkinter import *
+import speedtest
+
+def speedcheck():
+    st = speedtest.Speedtest()
+    st.get_servers()
+    down = str(round(st.download()/(10**6),3))+"Mbps"
+    up = str(round(st.upload() /(10**6),3))+"Mbps"
+    lab_down.config(text=down)
+    lab_up.config(text=up)
+
+sp = Tk()
+sp.title("Ak's Internet's Speed")
+sp.geometry("500x500")
+sp.config(bg="Blue")
+
+lab = Label(sp, text="Internet's Speed", font=("Times new roman", 60, "bold"), bg="blue", fg="White")
+lab.place(x=35, y=40)
+
+download_label = Label(sp, text="Download Speed", font=("Times new roman", 30, "bold"), bg="black", fg="White")
+download_label.place(x=50, y=150, height=30, width=380)
+
+lab_down = Label(sp, text="00", font=("Times new roman", 20, "bold"), bg="black", fg="White")
+lab_down.place(x=50, y=190, height=30, width=380)
+
+upload_label = Label(sp, text="Upload Speed", font=("Times new roman", 30, "bold"), bg="Sky Blue", fg="White")
+upload_label.place(x=50, y=230, height=30, width=380)
+
+lab_up = Label(sp, text="00", font=("Times new roman", 20, "bold"), bg="Sky blue", fg="White")
+lab_up.place(x=50, y=270, height=30, width=380)
+
+button = Button(sp, text="Check Speed", font=("Times new roman", 20, "bold"), relief=RAISED, command=speedcheck)
+button.place(x=50, y=400, height=30, width=380)
+
+sp.mainloop()
+
+
+
+
+"""
 import speedtest
 s=speedtest.Speedtest()
 print("What do you want to Test:\n1) Upload Speed\n2) Download Speed\n3) Ping\n")
@@ -14,3 +54,4 @@ elif option==3:
     print("Ping: {0}ms".format(s.results.ping))
 else:
     print("Invalid Option")
+"""
--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 from tkinter import *
 import speedtest
+import st
+
 
 def speedcheck():
-    ak = speedtest.Speedtest()
-    ak.get_servers()
-    down = str(round(ak.download()/(10**6),3))+"Mbps"
-    up = str(round(ak.upload()/(10 ** 6),3))+"Mbps"
+    st = speedtest.Speedtest()
+    st.get_servers()
+    down = str(round(st.download()))  # /(10**6),3))+"Mbps"
+    up = str(round(st.upload()))  # /(10**6),3))+"Mbps"
     lab_down.config(text=down)
-    lab_up.config(text = up)
-ak = Tk()
-ak.title("Ak's Internet's Speed")
-ak.geometry("500x500")
-ak.config(bg ="Blue")
+    lab_up.config(text=up)
 
 
-lab = Label(ak, text = "Internet's Speed",font = ("Times new roman",60,"bold"),bg = "blue", fg ="White")
-lab.place(x= 35, y =40)
+sp = Tk()
+sp.title("Ak's Internet's Speed")
+sp.geometry("500x500")
+sp.config(bg="Blue")
 
-lab = Label(ak, text = "Download Speed",font = ("Times new roman",30,"bold"),bg = "black", fg ="White")
-lab.place(x= 50, y =150, height = 30, width = 380)
+lab = Label(sp, text="Internet's Speed", font=("Times new roman", 60, "bold"), bg="blue", fg="White")
+lab.place(x=35, y=40)
 
-lab_down = Label(ak, text = "00",font = ("Times new roman",20,"bold"),bg = "black", fg ="White")
-lab_down.place(x= 50, y =190,height = 30,width = 380)
+download_label = Label(sp, text="Download Speed", font=("Times new roman", 30, "bold"), bg="black", fg="White")
+download_label.place(x=50, y=150, height=30, width=380)
 
-lab = Label(ak, text = "Upload Speed",font = ("Times new roman",30,"bold"),bg = "Sky Blue", fg ="White")
-lab.place(x= 50, y =230,height = 30,width = 380)
+lab_down = Label(sp, text="00", font=("Times new roman", 20, "bold"), bg="black", fg="White")
+lab_down.place(x=50, y=190, height=30, width=380)
 
-lab_up = Label(ak, text = "00",font = ("Times new roman",20,"bold"),bg = "Sky blue", fg ="White")
-lab_up.place(x= 50, y =270,height = 30,width = 380)
+upload_label = Label(sp, text="Upload Speed", font=("Times new roman", 30, "bold"), bg="Sky Blue", fg="White")
+upload_label.place(x=50, y=230, height=30, width=380)
 
-button = Button(ak,text="Check Speed",font = ("Times new roman",20,"bold"),relief= RAISED,command=speedcheck)
-button.place(x= 50, y =400,height = 30,width = 380)
+lab_up = Label(sp, text="00", font=("Times new roman", 20, "bold"), bg="Sky blue", fg="White")
+lab_up.place(x=50, y=270, height=30, width=380)
 
-ak.mainloop()
+button = Button(sp, text="Check Speed", font=("Times new roman", 20, "bold"), relief=RAISED, command=speedcheck)
+button.place(x=50, y=400, height=30, width=380)
+
+sp.mainloop()
+
+if __name__ == '__speedtest__':
+    speedcheck()
